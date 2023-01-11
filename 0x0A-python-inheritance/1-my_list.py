103c688259266c5879bf40c70af65ca4bd36e408
maru <i.marusisay@gmail.com>
2023-01-11 07:47:26 +0300

    pushing 0x0A python inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-my_list.py
+"""Defines an inherited list class MyList."""
+
+
+class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
