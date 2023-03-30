712432f2f2c7395c7d350da455cb70ce8d6f2da1
Maru <i.marusisay@gmail.com>
2023-03-30 13:16:07 +0300

    3rd push

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s -X GET "$1" -H "X-HolbertonSchool-User-Id: 98"
+# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id-User-Id: 98" "$1"
